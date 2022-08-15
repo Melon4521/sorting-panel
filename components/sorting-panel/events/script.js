16a6e1df0142f224a36aea40644f1cf452868f0c
xleb321 <vlad123kyz@gmail.com>
2022-08-15 11:52:31 +0300

    version 0.01

--- components/sorting-panel/events/script.js
+++ components/sorting-panel/events/script.js
@@ -3,10 +3,10 @@ export default function sortingEvents() {
         maxPrice = document.getElementById('MaxPriceCard').attributes.value.value;
 
     for (let i = 0; i < allItem.children.length; i++) {
-
         console.log(i, Number(allItem.children[i].attributes[3].value) > Number(maxPrice), Number(allItem.children[i].attributes[3].value), Number(maxPrice));
         if (Number(allItem.children[i].attributes[3].value) > Number(maxPrice)) {
             allItem.children[i].style.display='none';
         };
     };
+
 };
