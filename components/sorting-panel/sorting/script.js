0714aba9b32f669fcb8ee6916b96f36d413b571a
xleb321 <gleb.sazonov.58@mail.ru>
2022-08-18 21:40:01 +0300

    SortPanel v1.0
    Sorting panel working on 100%

--- components/sorting-panel/sorting/script.js
+++ components/sorting-panel/sorting/script.js
@@ -1,14 +1,38 @@
 export default function sortingEvents() {
 
     let allItem = document.getElementById('Right'),
-        maxPrice = document.getElementById('MaxPriceCard').textContent;
+        maxPrice = document.getElementById('MaxPriceCard').textContent,
+        select = document.getElementById('selectGroup');
+
+    let arrAll = [];
+
+    for (let i = 0; i < select.children.length; i++) {
+        let type = select.children[i].children[0].children[0].children[2].value,
+            qualification = 'data-' + select.children[i].dataset.name;
+
+        if ((type != 0)) {
+            for (let x = 0; x < allItem.children.length; x++) {
+                if (allItem.children[x].getAttribute(qualification) != type) {
+                    arrAll.push(x);
+                }
+            };
+        };
+    };
 
     for (let i = 0; i < allItem.children.length; i++) {
-        // console.log(i, Number(allItem.children[i].attributes[3].value) > Number(maxPrice), Number(allItem.children[i].attributes[3].value), Number(maxPrice));
         if (Number(allItem.children[i].attributes[3].value) > Number(maxPrice)) {
-            allItem.children[i].style.display = 'none';
-        } else {
-            allItem.children[i].style.display = 'block';
+            arrAll.push(i);
         };
+    };
+
+    arrAll = Array.from(new Set(arrAll)) // Удаление лишнего (повторов)
+    arrAll.sort((a, b) => a - b); // Сортировка по возрастанию
+
+    for (let i = 0; i < allItem.children.length; i++) {
+        allItem.children[i].style.display = 'block';
+    };
+
+    for (let i = 0; i < arrAll.length; i++) {
+        allItem.children[arrAll[i]].style.display = 'none';
     };
 };
