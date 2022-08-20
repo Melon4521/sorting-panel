4975cbcc245e2701c62005997547aff0ef3946d2
xleb321 <vlad123kyz@gmail.com>
2022-08-20 17:21:57 +0300

    Shopping cart v0.95

--- components/shopping-cart/script.js
+++ components/shopping-cart/script.js
@@ -47,7 +47,7 @@ export default function cartMainFunction() {
         };
 
         setCartData(cartData);
-        changeCartIconNumber()
+        changeCartIconNumber();
     };
 
     function generateCartCard(cardAtrs) {
